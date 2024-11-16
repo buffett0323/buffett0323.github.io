d998ad608cb3fc4cda858c1e7b3811ad3ce2d68b
buffett0323 <philip910323@gmail.com>
2024-11-16 12:47:31 +0800

    Init settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,20 +16,20 @@ export default function App({ Component, pageProps }: CustomAppProps) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
       <Head>
-        <title>Chun-Mao Lai</title>
+        <title>Jeng-Yue Liu</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#F1F1F1" />
         <meta name="description" content="Jeng-Yue Liu's personal website" />
         <meta property="og:image" content="/favicon.ico" />
         <meta property="og:image:type" content="image/png" />
-        <meta property="og:url" content="www.mecoli.net" />
-        <meta property="og:title" content="Chun-Mao Lai" />
-        <meta property="og:description" content="Chun-Mao (Michael) Lai's personal website" />
+        {/* <meta property="og:url" content="www.mecoli.net" /> */}
+        <meta property="og:title" content="Jeng-Yue Liu" />
+        <meta property="og:description" content="Jeng-Yue (Buffett) Liu's personal website" />
         <meta httpEquiv="Permissions-Policy" content="interest-cohort=()"></meta>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" /> */}
         {/* <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.4/css/bulma.min.css" /> */}
       </Head>
       {Component.transparentNavbar ? (
