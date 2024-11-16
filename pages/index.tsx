ea82bc404784787be4a418ff3086c0c0a5c1f40e
buffett0323 <philip910323@gmail.com>
2024-11-16 13:02:59 +0800

    index_navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 
-const Home = () => {
+function Home() {
+
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    document.body.style.userSelect = 'none';
+  }, []);
+
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>Welcome to My Website</h1>
-      <p>Hi, I'm Buffett Liu! Welcome to my portfolio.</p>
-      <Link href="/about">About Me</Link>
-    </div>
+    <>
+      <div className="p-30 text-center">
+        <div className="absolute bottom-0 left-0 flex w-full flex-col flex-nowrap items-center justify-end py-0">
+          <Link
+            id="enter"
+            className="mb-70p text-center text-4xl text-gray-700 duration-500 hover:scale-125 hover:cursor-pointer dark:text-white"
+            href="/about"
+          >
+            Enter
+          </Link>
+        </div>
+      </div>
+      {/* <ParticleText text={particleText} /> */}
+    </>
   );
-};
+}
+Home.transparentNavbar = true;
 
 export default Home;
