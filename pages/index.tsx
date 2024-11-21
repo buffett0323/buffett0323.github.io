4377212a97d32dc2795adbad3de1ecc6e6dd6dde
buffett0323 <philip910323@gmail.com>
2024-11-21 13:10:28 +0800

    Index page changing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,14 @@
-import React, { useEffect } from 'react';
-import Link from 'next/link';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function Home() {
-
+  const router = useRouter();
 
   useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    document.body.style.userSelect = 'none';
-  }, []);
+    router.replace('/about');
+  }, [router]);
 
-  return (
-    <>
-      <div className="p-30 text-center">
-        <div className="absolute bottom-0 left-0 flex w-full flex-col flex-nowrap items-center justify-end py-0">
-          <Link
-            id="enter"
-            className="mb-70p text-center text-4xl text-gray-700 duration-500 hover:scale-125 hover:cursor-pointer dark:text-white"
-            href="/about"
-          >
-            Enter
-          </Link>
-        </div>
-      </div>
-    </>
-  );
+  return null;
 }
-Home.transparentNavbar = true;
 
 export default Home;
