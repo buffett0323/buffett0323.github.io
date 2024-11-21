6a83d392558ba5302654be95740cd327492a0863
buffett0323 <philip910323@gmail.com>
2024-11-21 13:12:54 +0800

    Knowledge exp

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -11,14 +11,13 @@ function Experience() {
         <h1 className="pb-5 pt-10 text-center text-4xl font-bold sm:text-5xl">Experience.</h1>
         <div className="text-container mx-auto max-w-md border-t-2 border-gray-400 pt-2">
           <p className="pb-5 text-center text-xl leading-normal text-gray-600 dark:text-gray-300">
-            The only source of knowledge is experience.
+            Knowledge gives us the tools to dream, and experience gives us the courage to achieve those dreams.
           </p>
         </div>
         <div className="mx-auto mt-10 box-border flex w-full flex-col items-center space-y-16 px-5 max-sm:px-0">
             <Work />
             <Project />
             <Research />
-            {/* <Extra /> */}
             <Skills />
         </div>
       </div>
