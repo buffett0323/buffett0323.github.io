d622f91c45539dbaee419eebd77600c0af25edc8
buffett0323 <philip910323@gmail.com>
2025-01-06 13:22:43 +0800

    research interest

--- components/Experience/Research_Interest.tsx
+++ components/Experience/Research_Interest.tsx
@@ -4,11 +4,16 @@ import { researchInterests } from '../../constants/experience';
 export default function Research_Interest() {
   return (
     <div className="w-full rounded-lg bg-white px-2 py-12 shadow-md dark:bg-gray-600 sm:px-12">
-      <h1 className="mb-8 text-center text-3xl font-bold">{'Research Interest'}</h1>
-      <div className="mt-12 w-full rounded-lg bg-white px-4 py-6 shadow-md dark:bg-gray-600">
-        <ul className="list-inside list-disc text-gray-700 dark:text-gray-300">
+      <h1 className="mb-8 text-center text-3xl font-bold">
+        {'Research Interest'}
+      </h1>
+      <div className="mt-12 w-full rounded-lg bg-white px-6 py-8 shadow-lg dark:bg-gray-800">
+        <ul className="grid grid-cols-1 gap-4 text-gray-800 dark:text-gray-300 sm:grid-cols-2 md:grid-cols-3">
           {researchInterests.map((interest, idx) => (
-            <li key={idx} className="mb-2 text-lg max-sm:text-base">
+            <li
+              key={idx}
+              className="flex items-center justify-center rounded-lg bg-gray-100 p-4 text-center text-lg font-semibold text-gray-700 shadow-sm hover:scale-105 hover:bg-blue-200 dark:bg-gray-700 dark:hover:bg-indigo-500 dark:text-gray-100"
+            >
               {interest}
             </li>
           ))}
--- pages/project.tsx
+++ pages/project.tsx
@@ -1,5 +1,5 @@
 import Selected_Projects from '../components/Experience/Projects';
-import Research_Interest from '../components/Experience/Research_Interest';
+// import Research_Interest from '../components/Experience/Research_Interest';
 
 function Project() {
   return (
@@ -7,7 +7,7 @@ function Project() {
       <div className="mx-auto flex w-screen flex-col px-5 md:w-2xl lg:w-3xl xl:w-4xl">
         <h1 className="pb-5 pt-10 text-center text-4xl font-bold sm:text-5xl">Selected Projects</h1>
         <div className="mx-auto mt-10 box-border flex w-full flex-col items-center space-y-16 px-5 max-sm:px-0">
-            <Research_Interest />
+            {/* <Research_Interest /> */}
             <Selected_Projects />
         </div>
       </div>
