ab90005ee7dc9ad23a633329fcc4180a558b6022
buffett0323 <philip910323@gmail.com>
2025-12-04 18:42:19 -0500

    Google scholar update

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -108,8 +108,28 @@ export default function Navbar() {
               </a>
 
               <a
-                href={generalData.facebook}
-                aria-label="Facebook"
+                href={generalData.googleScholar}
+                aria-label="Google Scholar"
+                className="rounded-lg p-2 hover:bg-white hover:shadow-lg hover:dark:bg-gray-800"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-mortarboard h-5 w-5"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3.5a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.5 8 2.54l6.242 2.96L8 8.46Z"/>
+                  <path d="M4.176 9.032a.5.5 0 0 1 .292.643l-1.5 4a.5.5 0 1 1-.936-.35l1.5-4a.5.5 0 0 1 .644-.293Zm7.648 0a.5.5 0 0 1 .644.293l1.5 4a.5.5 0 1 1-.936.35l-1.5-4a.5.5 0 0 1 .292-.643Z"/>
+                </svg>
+              </a>
+
+              <a
+                href={generalData.linkedin}
+                aria-label="Linkedin"
                 className="rounded-lg p-2 hover:bg-white hover:shadow-lg hover:dark:bg-gray-800"
               >
                 <svg
@@ -117,10 +137,10 @@ export default function Navbar() {
                   width="16"
                   height="16"
                   fill="currentColor"
-                  className="bi bi-facebook h-5 w-5"
+                  className="bi bi-linkedin h-5 w-5"
                   viewBox="0 0 16 16"
                 >
-                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                 </svg>
               </a>
 
@@ -142,8 +162,8 @@ export default function Navbar() {
               </a>
 
               <a
-                href={generalData.linkedin}
-                aria-label="Linkedin"
+                href={generalData.facebook}
+                aria-label="Facebook"
                 className="rounded-lg p-2 hover:bg-white hover:shadow-lg hover:dark:bg-gray-800"
               >
                 <svg
@@ -151,10 +171,10 @@ export default function Navbar() {
                   width="16"
                   height="16"
                   fill="currentColor"
-                  className="bi bi-linkedin h-5 w-5"
+                  className="bi bi-facebook h-5 w-5"
                   viewBox="0 0 16 16"
                 >
-                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                 </svg>
               </a>
 
